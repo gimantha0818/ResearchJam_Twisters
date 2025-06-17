2dd5cf6d122beb604857530878d403e7f526677e
Gimantha <157255111+gimantha0818@users.noreply.github.com>
2025-06-17 11:25:21 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,35 @@
-**Stepscan Live**
+# **Stepscan Live**
 
-Pressure data. Serial ID of the tile can be identified from the API. 
+# Pressure data. Serial ID of the tile can be identified from the API. 
+# Install stepscanlive.msi
+# DAQ.exe can be interfaced to the UI (C++ based) in Example Apps folder. 
 
-Install stepscanlive.msi
+# DAQ.exe has two configutation files 
+# 1)DAQ.confiq is autogrenerated by stepscan live. 
+# 2)Tilelookup -> serialnumbers.dat (the API will need the 12 tile associated lookup files). 
+# So you would use the API in a loop. 
 
-DAQ.exe can be interfaced to the UI (C++ based) in Example Apps folder. 
+# E.g.: 
+# while(TRUE) 
+# api.read(data)
 
-DAQ.exe has two configutation files 
-1)DAQ.confiq is autogrenerated by stepscan live. 
-2)Tilelookup -> serialnumbers.dat (the API will need the 12 tile associated lookup files). 
+# titleReader.read()
 
-So you would use the API in a loop. 
+# Colours:
+# Red, Green and Blue
 
-E.g.: 
-while(TRUE) 
-api.read(data)
+# split the two tile array to 3 colours. 720x240 dimensional block. 
+# 1 player only. Get from point A to point B. 
 
-titleReader.read()
+# Steps:
+# 1)Starting position
 
+# 2)Randomize new action -> instructions displayed on Screen UI. 
 
-Colours:
-Red, Green and Blue
+# 3)Check correctness. 
 
-split the two tile array to 3 colours. 720x240 dimensional block. 
-1 player only. Get from point A to point B. 
+# 4)Limb phase -> contact or no-contact. 10kPa threshold for contact detection. 
 
-Steps:
-1)Starting position
+# 5)Allowed to move and not allowed to move and error of action -> Disqualified. 
 
-2)Randomize new action -> instructions displayed on Screen UI. 
-
-3)Check correctness. 
-
-4)Limb phase -> contact or no-contact. 10kPa threshold for contact detection. 
-
-5)Allowed to move and not allowed to move and error of action -> Disqualified. 
-
-6)End -> Passed!
+# 6)End -> Passed!
